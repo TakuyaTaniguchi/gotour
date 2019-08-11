6f8e9ad7ca87553c049ac54ded5802c598f6b273
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-08-11 14:52:37 +0900

    Go Tour

---
+++ if.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	fmt.Println(2, -4)
+}
